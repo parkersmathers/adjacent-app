8f806303c3e71cf6aebb20845f2c676291947eee
parkersmathers <parker.smathers@gmail.com>
2019-03-25 09:15:40 -0400

    initial setup for using React state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import { createData } from './mockData'
+import React, { useState } from 'react'
+import CreateTargetForm from './CreateTargetForm'
+import { getData } from './mockData'
+import faker from 'faker'
 import './App.css'
 
 const App = () => {
-  const companyData = createData(10)
+  const companiesData = getData()
+
+  const [companies, setCompanies] = useState(companiesData)
+
+  const createTarget = company => {
+    company.companyId = faker.random.uuid()
+    setCompanies([...companies, company])
+  }
 
   return (
     <div className='App'>
@@ -16,32 +25,41 @@ const App = () => {
           <header>
             <h2>Companies</h2>
           </header>
+          <a href='#form'>New</a>
           <ul>
-            {companyData.map((c, i) => (
-              <li key={i}>
-                {/* <div>
-                  <p>{}</p>
-                </div> */}
-                <div>
-                  <p>{c.companyCard.company.name}</p>
-                  <p>{c.companyCard.address.city}</p>
-                  <p>{c.companyCard.website}</p>
-                </div>
-                <div>
-                  <p>{c.contactName}</p>
-                  <p>{c.contactJobTitle}</p>
-                  <p>{c.contactPhone}</p>
-                  <p>{c.contactEmail}</p>
-                </div>
-                <div>
-                  <p>Market: {c.companyMarket}</p>
-                  <p>Employees: {c.companySize}</p>
-                  <p>Funding: ${c.companyFunding}</p>
-                </div>
-              </li>
-            ))}
+            {companies.map(c => (
+                <li key={c.companyId}>
+                  <div>
+                    <p>{c.companyCard.company.name}</p>
+                    <p>{c.companyCard.address.city}</p>
+                    <p>{c.companyCard.website}</p>
+                  </div>
+                  <div>
+                    <p>{c.contactName}</p>
+                    <p>{c.contactJobTitle}</p>
+                    <p>{c.contactPhone}</p>
+                    <p>{c.contactEmail}</p>
+                  </div>
+                  <div>
+                    <p>Market: {c.companyMarket}</p>
+                    <p>Employees: {c.companySize}</p>
+                    <p>Funding: ${c.companyFunding}</p>
+                  </div>
+                  <div>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </div>
+                  <div>
+                    <button>Researching</button>
+                    <button>Pending Approval</button>
+                    <button>Approved</button>
+                    <button>Declined</button>
+                  </div>
+                </li>
+              ))}
           </ul>
         </section>
+        <CreateTargetForm createTarget={createTarget} />
       </main>
       <div className='App-footer' />
     </div>
--- src/CreateTargetForm.js
+++ src/CreateTargetForm.js
@@ -1,88 +1,138 @@
 import React, { useState } from 'react'
 import './CreateTargetForm.css'
 
-const CreateTargetForm = () => {
+const CreateTargetForm = props => {
+  const initialFormState = {
+    companyId: null,
+    companyName: '',
+    companyLocation: '',
+    companyWebsite: '',
+    contactName: '',
+    contactJobtitle: '',
+    contactPhone: '',
+    contactEmail: '',
+    companyMarket: '',
+    companySize: '',
+    companyFunding: ''
+  }
+
+  const [company, setCompany] = useState(initialFormState)
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+
+    setCompany({...company, [name]: value})
+  }
+
   return (
-    <form id='form'>
+    <form
+      onSubmit={event => {
+        event.preventDefault()
+        if (!company.companyName || !company.contactName) return
+
+        props.createTarget(company)
+        setCompany(initialFormState)
+      }}
+    >
       <section>
         <h2>Company</h2>
-        <label for='company-name'>Name:</label>
+        <label>Name*:</label>
         <input
           type='text'
           id='company-name'
           name='company-name'
           placeholder='Company name'
+          value={company.companyName}
+          onChange={handleInputChange}
         />
-        <label for='company-location'>Location:</label>
+        <label htmlFor='company-location'>Location:</label>
         <input
           type='text'
           id='company-location'
           name='company-location'
           placeholder='City'
+          value={company.companyLocation}
+          onChange={handleInputChange}
         />
-        <label for='company-website'>Website:</label>
+        <label htmlFor='company-website'>Website:</label>
         <input
           type='text'
           id='company-website'
           name='company-website'
           placeholder='Company website'
+          value={company.companyWebsite}
+          onChange={handleInputChange}
         />
       </section>
 
       <section>
         <h2>Key Contact</h2>
-        <label for='contact-name'>Name:</label>
+        <label htmlFor='contact-name'>Name*:</label>
         <input
           type='text'
           id='contact-name'
           name='contact-name'
           placeholder='Contact name'
+          value={company.contactName}
+          onChange={handleInputChange}
         />
-        <label for='contact-jobtitle'>Job Title:</label>
+        <label htmlFor='contact-jobtitle'>Job Title:</label>
         <input
           type='text'
           id='contact-jobtitle'
           name='contact-jobtitle'
           placeholder='Contact job title'
+          value={company.contactJobtitle}
+          onChange={handleInputChange}
         />
-        <label for='contact-phone'>Phone:</label>
+        <label htmlFor='contact-phone'>Phone:</label>
         <input
           type='tel'
           id='contact-phone'
           name='contact-phone'
           placeholder='Contact phone'
+          value={company.contactPhone}
+          onChange={handleInputChange}
         />
-        <label for='contact-email'>Email:</label>
+        <label htmlFor='contact-email'>Email:</label>
         <input
           type='email'
           id='contact-email'
           name='contact-email'
           placeholder='Contact email'
+          value={company.contactEmail}
+          onChange={handleInputChange}
         />
       </section>
 
       <section>
         <h2>Financial Performance</h2>
-        <label for='performance-market'>Market:</label>
+        <label htmlFor='performance-market'>Market:</label>
         <input
           type='text'
           id='performance-market'
           name='performance-market'
           placeholder='Market'
+          value={company.companyMarket}
+          onChange={handleInputChange}
         />
-        <label for='performance-employees'>Job Title:</label>
+        <label htmlFor='performance-employees'>Job Title:</label>
         <input
           type='text'
           id='performance-employees'
           name='performance-employees'
           placeholder='Number of employees'
+          value={company.companySize}
+          onChange={handleInputChange}
         />
-        <label for='performance-funding'>Funding:</label>
+        <label htmlFor='performance-funding'>Funding:</label>
         <input
           type='number'
           id='performance-funding'
           name='performance-funding'
           placeholder='Total funding'
+          value={company.companyFunding}
+          onChange={handleInputChange}
         />
       </section>
 
